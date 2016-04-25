dafb2a8e86bcf192c02affc4cba6f95627411b27
Marc Vollmer <admin@vmtek.de>
2016-04-25 22:08:03 +0200

    Login & Logout

--- opendrivebackend.py
+++ opendrivebackend.py
@@ -1,25 +1,52 @@
-import duplicity.backend
+from duplicity import backend
 from duplicity import globals
 from duplicity import log
 from duplicity import util
 from duplicity.errors import BackendException
+import urllib2
+import json
 
 
-class OpenDriveBackend(duplicity.backend.Backend):
+class OpenDriveBackend(backend.Backend):
     """
     Backend to access OpenDrive storage services
     Contributed in 2016 by Marc Vollmer <admin@vmtek.de>
     """
 
     def __init__(self, parsed_url):
-        duplicity.backend.Backend.__init__(self, parsed_url)
+        backend.Backend.__init__(self, parsed_url)
         self.parsed_url = parsed_url
         self.username = parsed_url.username
         self.password = self.get_password()
         self.directory = parsed_url.path
+
         self.baseurl = "https://dev.opendrive.com/api/v1"
 
+        self.sessionid = None
+
         log.Info("Using OpenDrive Backend with directory ID %s" % self.directory)
+
+    def __dopostrequest(self, url, postobject):
+        """
+        Do a Post Request
+        :param url: URL to POST to
+        :param postobject: Object to POST (will be encoded as JSON)
+        :return: Response Object
+        """
+        postdata = json.dumps(postobject).encode('utf8')
+
+        req = urllib2.Request(url, data=postdata, headers={'content-type': 'application/json'})
+        req.get_method = lambda: 'POST'
+        return urllib2.urlopen(req)
+
+    def __decodejson(self, data):
+        """
+        Decode a JSON Object
+        :param data: Byte Data
+        :return: JSON Object
+        """
+        strdata = data.decode('utf8')
+        return json.loads(strdata)
 
     def _list(self):
         """
@@ -57,12 +84,50 @@ class OpenDriveBackend(duplicity.backend.Backend):
         """
         pass
 
+    def login(self):
+        """
+        Login to OpenDrive
+        """
+        try:
+            self.close()
+            log.Info("Logging in to OpenDrive")
+
+            loginurl = self.baseurl + "session/login.json"
+            logindata = {"username": self.username, "passwd": self.password}
+
+            resp = self.__dopostrequest(loginurl, logindata)
+            status = resp.getcode()
+            if status != 200:
+                log.FatalError("Login failed, API returned Status code: %d" % status)
+                raise BackendException("Error logging in to OpenDrive. API Returned Status %d" % status)
+
+            data = resp.read()
+            userinfo = self.__decodejson(data)
+            self.sessionid = userinfo["SessionID"]
+        except:
+            log.FatalError("Failed to login to OpenDrive")
+            raise BackendException("Error logging in to OpenDrive")
+
     def close(self):
         """
         Close the backend, releasing any resources held and
         invalidating any file objects obtained from the backend.
         """
-        pass
+        try:
+            if self.sessionid:
+                log.Info("Logout")
+
+                logouturl = self.baseurl + "session/logout.json"
+                logoutdata = {"session_id": self.sessionid}
+
+                resp = self.__dopostrequest(logouturl, logoutdata)
+                status = resp.getcode()
+                if status != 200:
+                    log.Warn("Logout failed, API Returned Status Code: %d" % status)
+
+                self.sessionid = None
+        except:
+            log.Warn("Logout failed")
 
 
-duplicity.backend.register_backend("opendrive", OpenDriveBackend)
+backend.register_backend("opendrive", OpenDriveBackend)
