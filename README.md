34037154c264f65fe552fdd51d31e034daf75f05
Marc Vollmer <admin@vmtek.de>
2016-04-25 21:19:39 +0200

    Class preparation

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # OpenDrive Duplicity Backend
 
 This is a duplicity backend for the cloud storage service OpenDrive.
+
+# Setup
+
+```
+# Linux
+cp opendrivebackend.py /usr/lib/python2.7/dist-packages/duplicity/backends/
+
+# Mac OS X
+cp opendrivebackend.py /Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages/duplicity/backends
+```
+
+# Usage
+```
+duplicity source_path opendrive://email@addr.tld:password@od/directoryid
+```
--- opendrivebackend.py
+++ opendrivebackend.py
@@ -2,7 +2,8 @@ import duplicity.backend
 from duplicity import globals
 from duplicity import log
 from duplicity import util
-from duplicity.errors import BackendException, FatalBackendException
+from duplicity.errors import BackendException
+
 
 class OpenDriveBackend(duplicity.backend.Backend):
     """
@@ -12,17 +13,56 @@ class OpenDriveBackend(duplicity.backend.Backend):
 
     def __init__(self, parsed_url):
         duplicity.backend.Backend.__init__(self, parsed_url)
+        self.parsed_url = parsed_url
+        self.username = parsed_url.username
+        self.password = self.get_password()
+        self.directory = parsed_url.path
+        self.baseurl = "https://dev.opendrive.com/api/v1"
+
+        log.Info("Using OpenDrive Backend with directory ID %s" % self.directory)
 
     def _list(self):
-        """List files in directory"""
+        """
+        Return list of filenames (byte strings) present in backend
+        """
+        pass
 
-    def _query(self, remote_filename):
-        """Get the size of the remote file"""
+    def get(self, remote_filename, local_path):
+        """Retrieve remote_filename and place in local_path"""
+        pass
 
-    def _get(self, remote_filename, local_path):
-        """Get remote filename, saving it to local_path"""
+    def put(self, source_path, remote_filename=None):
+        """
+        Transfer source_path (Path object) to remote_filename (string)
 
-    def _put(self, source_path, remote_filename=None):
-        """Transfer source_path to remote_filename"""
+        If remote_filename is None, get the filename from the last
+        path component of pathname.
+        """
+        pass
+
+    def delete(self, filename_list):
+        """
+        Delete each filename in filename_list, in order if possible.
+        """
+        pass
+
+    def _query_file_info(self, filename):
+        """
+        Return metadata about file
+
+        Supported metadata are:
+        'size': if >= 0, size of file
+                if -1, file is not found
+                if None, error querying file
+        """
+        pass
+
+    def close(self):
+        """
+        Close the backend, releasing any resources held and
+        invalidating any file objects obtained from the backend.
+        """
+        pass
+
 
 duplicity.backend.register_backend("opendrive", OpenDriveBackend)
